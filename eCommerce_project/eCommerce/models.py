462cbc8a02c7489c2df97b9619c9d5d454dd87d7
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-27 15:58:51 -0500

    started models

--- eCommerce_project/eCommerce/models.py
+++ eCommerce_project/eCommerce/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Users(models.Model): 
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 100)
+
+class Items(models.Model):
+    productType = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 100)
+    materials = 
+    images =
+    specifier =
+    price =
+    desc =
